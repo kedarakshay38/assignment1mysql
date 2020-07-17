4f5b892ac464fd42ba0482c333d6b82fb981ae45
kedarakshay38 <65217534+kedarakshay38@users.noreply.github.com>
2020-07-17 22:54:49 +0530

    Add files via upload

---
+++ assig 2,3,4,5/Q1.sql
@@ -0,0 +1,34 @@
+show databases;
+use spider;
+exit;
+show databases;
+use assignment1;
+show tables;
+exit;
+show databases;
+use assignment1;
+show tables;
+select * from orders;
+select onum as "order no",amt as"Amount",odate as "Date"
+ from orders;
+ 
+ show tables;
+ select * from salespeople;
+ select * from salespeople where snum='1001';
+ select * from customers ;
+ select * from customers where snum=1001;
+select * from salespeople; 
+select *from customeres;
+
+select city,sname,snum,comm from salespeople;
+select *from customers;
+select rating,city from customers where city=san jos;
+select *from customers;
+select  *from salespeople;
+select distinct sname from saepeople;
+select rating,cname from customers where city='san jose';
+SELECT DISTINCT
+    sname
+FROM
+    salespeople;
+    exit;
